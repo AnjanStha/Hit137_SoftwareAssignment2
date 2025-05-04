50ced99617f6001b56cf4801965c4f0f0c8cdb21
roshan-mahato <roshan3868@gmail.com>
2025-05-04 21:12:20 +1000

    modified:   Q1.py

--- Q1.py
+++ Q1.py
@@ -1,19 +1,24 @@
+# Checking raw_text.txt exists or not.
 def check_file():
     try:
-
+        # try opening file
         with open('raw_text.txt', 'r') as file:
+            # return file content
             return file.read()
-
+    
     except FileNotFoundError:
-
+        # if file not found, the program exit with error.
         print('File \'raw_text.txt\' not found')
         exit(1)
 
+# Checks user input ensuring value return as integer, if not default value 0 is returned.
 def check_input(user_input):
     try:
+        # return integer value if it is whole number
         if user_input.isdigit():
             return int(user_input)
         
+        # converting float to int
         float_number = float(user_input)
         int_number = int(round(float_number, 0))
 
@@ -22,6 +27,8 @@ def check_input(user_input):
         return int_number
     
     except ValueError:
+        # converting words or character to integer value.
+
         print(f'Converting character input \'{user_input}\' to ascii value...\n')
         ascii_value = [ord(ch) for ch in user_input]
         sum_ascii = sum(ascii_value)
@@ -31,37 +38,45 @@ def check_input(user_input):
         else:
             print(f'Could not convert \'{user_input}\' to ascii value. Default value is 0\n')
             return 0
-        
+
+# encrypting text using keys entered by user
 def text_encryption(msg, first_key, second_key):
 
     enc_text = ''
     to_shift = 0
 
     for ch in msg:
+        # check single character is upper case
         if ch.isupper():
-
+            # ch is/between upper case A and M
             if ord(ch) >= ord('A') and ord(ch) <= ord('M'):
 
                 to_shift = first_key
+                # shift backward by first entered key
                 enc_text = enc_text + chr(((ord(ch) - ord('A') - to_shift) % 26) + ord('A'))
 
+            # ch is/between upper case N and Z
             elif ord(ch) >= ord('N') and ord(ch) <= ord('Z'):
 
                 to_shift = second_key ** 2
+                # shift forward 
                 enc_text = enc_text + chr(((ord(ch) - ord('A') + to_shift ) % 26) + ord('A'))
-        
-        elif ch.islower():
 
+        # check single character is lower case
+        elif ch.islower():
+            # ch is/between lower case a and m
             if ord(ch) >= ord('a') and ord(ch) <= ord('m'):
 
                 to_shift = first_key * second_key
                 enc_text = enc_text + chr(((ord(ch) - ord('a') + to_shift) % 26) + ord('a'))
-            
+
+            # ch is/between lower case n and m
             elif ord(ch) >= ord('n') and ord(ch) <= ord('z'):
 
                 to_shift = first_key + second_key
                 enc_text = enc_text + chr(((ord(ch) - ord('a') - to_shift) % 26) + ord('a'))
-        
+
+        # space, commas, point as it is in original text
         else:
             enc_text = enc_text + ch
         
@@ -70,16 +85,20 @@ def text_encryption(msg, first_key, second_key):
 def main():
     data = None
 
+    # set file content to data
     data = check_file()
     
+    # continue if data is not empty.
     try:
         if data is not None:
             first_key = check_input(input('Enter First key: '))
             second_key = check_input(input('Enter Second key: '))
-            # print(first_key, second_key)
-
+        
+            # encrypted text 
             text_to_write = text_encryption(data, first_key, second_key)
+            # print(text_to_write)
 
+            # creating encrypted_text.txt and writing encrypted text
             with open('encrypted_text.txt', 'w+') as file:
                 print(f'\nWriting encrypted text to \'encrypted_text.txt\'')
                 file.write(text_to_write)
@@ -88,5 +107,6 @@ def main():
     except Exception as error:
         raise error
 
+# runs only if this python file executed.
 if __name__ == '__main__':
     main()
